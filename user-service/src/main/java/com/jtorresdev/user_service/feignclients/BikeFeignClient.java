6061890bfea878aafa761f676cff98543c378ed1
jcarlostoso <jcarlostoso@gmail.com>
2025-04-10 18:59:55 -0600

    fix: delete url on FeignClient

--- user-service/src/main/java/com/jtorresdev/user_service/feignclients/BikeFeignClient.java
+++ user-service/src/main/java/com/jtorresdev/user_service/feignclients/BikeFeignClient.java
@@ -8,8 +8,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
-
+/*
+//Como se implementa eureka ya no se usa la url, con el "name" debe funcionar
 @FeignClient(name = "bike-server", url = "http://localhost:8003/bike")
+*/
+@FeignClient(name = "bike-server")
 public interface BikeFeignClient {
     @PostMapping()
     Bike save(@RequestBody Bike bike);
