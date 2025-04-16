d21f69b1613ee26d1e09ad94d40b221eb94079ea
jcarlostoso <jcarlostoso@gmail.com>
2025-04-15 18:22:25 -0600

    add init branch

---
+++ gateway-service/src/test/java/com/jtorresdev/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtorresdev.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
